8e95475aef9401ecdf7a5998a51ce7f6825be1fb
yacitus <daryl.spitzer@gmail.com>
2019-09-14 21:57:01 -0700

    AuctionList implemented

--- frontend/src/components/AuctionList.js
+++ frontend/src/components/AuctionList.js
@@ -3,11 +3,23 @@ import PropTypes from "prop-types";
 import NoData from "./NoData";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Table from "react-bootstrap/Table";
 
 class AuctionList extends Component {
   static propTypes = {
     auctions: PropTypes.array.isRequired
   };
+
+  renderAuction(auction, index) {
+    return (
+      <tr key={index}>
+        <td>{auction.id}</td>
+        <td>{auction.name}</td>
+        <td>{auction.yearRange}</td>
+        <td>{auction.active ? '✅' : '❌'}</td>
+      </tr>
+    )
+  }
 
   render() {
     const { auctions } = this.props;
@@ -17,7 +29,20 @@ class AuctionList extends Component {
     return (
       <Container>
         <Jumbotron>
-          <h1 className="header">NOT IMPLEMENTED...</h1>
+          <h1 className="header">Auctions</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Years</th>
+                <th>Active</th>
+              </tr>
+            </thead>
+            <tbody>
+              {auctions.map(this.renderAuction)}
+            </tbody>
+          </Table>
         </Jumbotron>
       </Container>
     );
