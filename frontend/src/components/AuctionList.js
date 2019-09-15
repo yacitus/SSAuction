d8c0c15ad3977dfd71fa7b752be4f21b2a486e1e
yacitus <daryl.spitzer@gmail.com>
2019-09-15 12:41:48 -0700

    switch AuctionList to use react-bootstrap-table2

--- frontend/src/components/AuctionList.js
+++ frontend/src/components/AuctionList.js
@@ -2,48 +2,47 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import NoData from "./NoData";
 import Container from "react-bootstrap/Container";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Table from "react-bootstrap/Table";
+import BootstrapTable from 'react-bootstrap-table-next';
 
 class AuctionList extends Component {
   static propTypes = {
     auctions: PropTypes.array.isRequired
   };
 
-  renderAuction(auction, index) {
-    return (
-      <tr key={index}>
-        <td>{auction.id}</td>
-        <td>{auction.name}</td>
-        <td>{auction.yearRange}</td>
-        <td>{auction.active ? '✅' : '❌'}</td>
-      </tr>
-    )
-  }
-
   render() {
     const { auctions } = this.props;
 
     if (auctions.length === 0) return <NoData />;
 
+    function activeFormatter(cell, row) {
+      return (cell ? '✅' : '❌');
+    }
+
+    const columns = [{
+      dataField: 'id',
+      text: 'ID'
+    }, {
+      dataField: 'name',
+      text: 'Name'
+    }, {
+      dataField: 'yearRange',
+      text: 'Years'
+    }, {
+      dataField: 'active',
+      text: 'Active',
+      formatter: activeFormatter
+    }];
+
     return (
       <Container>
-        <Jumbotron>
-          <h1 className="header">Auctions</h1>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Years</th>
-                <th>Active</th>
-              </tr>
-            </thead>
-            <tbody>
-              {auctions.map(this.renderAuction)}
-            </tbody>
-          </Table>
-        </Jumbotron>
+        <h1 className="header">Auctions</h1>
+        <BootstrapTable
+          bootstrap4={ true }
+          keyField='id'
+          data={ auctions }
+          columns={ columns }
+          striped
+          hover />
       </Container>
     );
   }
