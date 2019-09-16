85236cf8fa88d4171d06f5961a41c91a0fc372e2
yacitus <daryl.spitzer@gmail.com>
2019-09-15 19:09:53 -0700

    switched Auction info to use Table (and installed moment & react-moment, which I'm not using yet--but I expect I will)

--- frontend/src/pages/Auction.js
+++ frontend/src/pages/Auction.js
@@ -5,6 +5,22 @@ import Error from "../components/Error";
 import Loading from "../components/Loading";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Table from "react-bootstrap/Table";
+// import Moment from 'react-moment';
+
+function seconds_to_days_hours_mins_secs_str(seconds)
+{ // day, h, m and s
+  var days     = Math.floor(seconds / (24*60*60));
+      seconds -= days    * (24*60*60);
+  var hours    = Math.floor(seconds / (60*60));
+      seconds -= hours   * (60*60);
+  var minutes  = Math.floor(seconds / (60));
+      seconds -= minutes * (60);
+  return ( (0<days)?(days+" day "):""
+          +(0<hours)?(hours+" hours "):""
+          +(0<minutes)?(minutes+" min "):""
+          +(0<seconds)?(seconds+" sec"):"" );
+}
 
 const GET_AUCTION_QUERY = gql`
   query GetAuction($auction_id: Int!) {
@@ -13,6 +29,9 @@ const GET_AUCTION_QUERY = gql`
       name
       yearRange
       active
+      nominationsPerTeam
+      secondsBeforeAutonomination
+      bidTimeoutSeconds
     }
   }
 `;
@@ -32,9 +51,37 @@ class Auction extends Component {
             <Container>
               <Jumbotron>
                 <h1 className="header">{data.auction.name}</h1>
-                <p>Years: {data.auction.yearRange}</p>
-                <p>Active: {data.auction.active ? '✅' : '❌'}</p>
               </Jumbotron>
+              <Table striped bordered>
+                <tbody>
+                  <tr>
+                    <td>Active:</td>
+                    <td>{data.auction.active ? '✅' : '❌'}</td>
+                  </tr>
+                  <tr>
+                    <td>Years:</td>
+                    <td>{data.auction.yearRange}</td>
+                  </tr>
+                  <tr>
+                    <td>Nominations Per Team:</td>
+                    <td>{data.auction.nominationsPerTeam}</td>
+                  </tr>
+                  <tr>
+                    <td>Time Before Auto-nomination:</td>
+                    <td>
+                      {seconds_to_days_hours_mins_secs_str(
+                          data.auction.secondsBeforeAutonomination)}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Time Before Bids Expire:</td>
+                    <td>
+                      {seconds_to_days_hours_mins_secs_str(
+                          data.auction.bidTimeoutSeconds)}
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
             </Container>
           );
         }}
