3cd383016991d8bc2d8f7e1be07ab6f716da64ee
Daryl Spitzer <dspitzer@roku.com>
2021-02-21 16:13:17 -0800

    this should've been run before the changes to Auction was pushed

--- backend/priv/repo/migrations/20210103215400_create_bid_logs.exs
+++ backend/priv/repo/migrations/20210103215400_create_bid_logs.exs
@@ -20,21 +20,21 @@ defmodule Ssauction.Repo.Migrations.CreateBidLogs do
       timestamps()
     end
 
-    flush()
+    # flush()
 
-    Repo.all(RosteredPlayer)
-    |> Enum.each(fn rp ->
-                   rp = Repo.preload(rp, [:player])
-                   auction = Repo.get!(Auction, rp.auction_id)
-                   %BidLog{}
-                   |> BidLog.changeset(%{amount: rp.cost,
-                                         type: "R",
-                                         datetime: auction.started_or_paused_at})
-                   |> Ecto.Changeset.put_assoc(:auction, auction)
-                   |> Ecto.Changeset.put_assoc(:team, Repo.get!(Team, rp.team_id))
-                   |> Ecto.Changeset.put_assoc(:player, rp.player)
-                   |> Repo.insert()
-                 end)
+    # Repo.all(RosteredPlayer)
+    # |> Enum.each(fn rp ->
+    #                rp = Repo.preload(rp, [:player])
+    #                auction = Repo.get!(Auction, rp.auction_id)
+    #                %BidLog{}
+    #                |> BidLog.changeset(%{amount: rp.cost,
+    #                                      type: "R",
+    #                                      datetime: auction.started_or_paused_at})
+    #                |> Ecto.Changeset.put_assoc(:auction, auction)
+    #                |> Ecto.Changeset.put_assoc(:team, Repo.get!(Team, rp.team_id))
+    #                |> Ecto.Changeset.put_assoc(:player, rp.player)
+    #                |> Repo.insert()
+    #              end)
   end
 
   def down do
